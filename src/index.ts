76ce862b907a80039e71422dbde88e986c820122
m0a <abe00makoto@gmail.com>
2019-12-22 18:02:02 +0900

    react追加

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,2 @@
-console.log('hello ts');
-
-const el = document.querySelector("#app");
-if (el) {
-    const anchr = document.createElement("a");
-    anchr.text = "hello typescript";
-    anchr.href = "https://www.typescriptlang.org/play/";
-    el.appendChild(anchr)
-}
+import render from "./components/index";
+render();
